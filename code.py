a5c5609662385610bc64648f4399b130f42d89e4
Nitin <55016899+logicaldemon@users.noreply.github.com>
2019-10-09 16:43:58 +0530

    Update code.py

--- code.py
+++ code.py
@@ -23,3 +23,28 @@ plt.imshow(image.values.reshape(28,28))                 #since image are of 28*2
 #%%
 train,test=train_test_split(df,test_size=0.2,random_state=12)
 del df
+train,test=train_test_split(df,test_size=0.2,random_state=12)
+del df
+
+
+#%%
+def x_and_y(df):
+    x=df.drop(['label'],axis=1)      #dropping of unnecessary columns
+    y=df.label 
+    return x,y
+x_train,y_train=x_and_y(train)
+x_test,y_test=x_and_y(test)
+
+#%%
+y_train.head()
+
+#%%
+x_test.head()
+
+#%%
+model=RandomForestClassifier(n_estimators=100,random_state=12)              #main implementation of decision tree in our trained data
+model.fit(x_train,y_train)
+prediction=model.predict(x_test)
+score=accuracy_score(y_test,prediction)
+print(score)
+
